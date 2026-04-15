0bc11e4a35c87d70d4518f62ad21ad774595a474
bimaswara <bimaswaraadam@gmail.com>
2026-04-15 11:29:06 +0700

    refactor: add stack balance asserts to analytics, banners, data, score
    These four modules were missing the `int top = lua_gettop(L)` /
    `assert(top == lua_gettop(L))` pattern used consistently in every other
    module. Without it, any future change that accidentally pushes or leaks
    a stack value would go undetected and cause silent Lua stack corruption.
    
    Functions returning values use `assert(top + 1 == lua_gettop(L))`.

--- yes2sdk/src/yes2sdk_banners.cpp
+++ yes2sdk/src/yes2sdk_banners.cpp
@@ -1,18 +1,24 @@
 #include "yes2sdk_banners.h"
 #if defined(DM_PLATFORM_HTML5)
 int Yes2SDKBanners::Show(lua_State* L) {
+    int top = lua_gettop(L);
     const char* id = luaL_checkstring(L, 1);
     const char* size = luaL_optstring(L, 2, "728x90");
     Yes2SDK_banners_show(id, size);
+    assert(top == lua_gettop(L));
     return 0;
 }
 int Yes2SDKBanners::Hide(lua_State* L) {
+    int top = lua_gettop(L);
     const char* id = luaL_checkstring(L, 1);
     Yes2SDK_banners_hide(id);
+    assert(top == lua_gettop(L));
     return 0;
 }
 int Yes2SDKBanners::HideAll(lua_State* L) {
+    int top = lua_gettop(L);
     Yes2SDK_banners_hideAll();
+    assert(top == lua_gettop(L));
     return 0;
 }
 #endif
--- yes2sdk/src/yes2sdk_score.cpp
+++ yes2sdk/src/yes2sdk_score.cpp
@@ -1,13 +1,17 @@
 #include "yes2sdk_score.h"
 #if defined(DM_PLATFORM_HTML5)
 int Yes2SDKScore::AddScore(lua_State* L) {
+    int top = lua_gettop(L);
     int score = luaL_checkinteger(L, 1);
     Yes2SDK_score_addScore(score);
+    assert(top == lua_gettop(L));
     return 0;
 }
 int Yes2SDKScore::SubmitScore(lua_State* L) {
+    int top = lua_gettop(L);
     const char* encrypted = luaL_checkstring(L, 1);
     Yes2SDK_score_submitScore(encrypted);
+    assert(top == lua_gettop(L));
     return 0;
 }
 #endif
